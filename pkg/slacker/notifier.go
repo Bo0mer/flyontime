d43532e875a069c994db9c2a0b9e82b233d89801
Ivan Borshukov <bo0merzzz@gmail.com>
2018-03-02 12:51:05 +0200

    pkg/slacker: add support for general and IM commands

--- pkg/slacker/notifier.go
+++ pkg/slacker/notifier.go
@@ -22,6 +22,7 @@ type Notifier struct {
 
 	initOnce sync.Once
 	slack    *slack.Client
+	selfID   string
 
 	commands  chan *flyontime.Command
 	callbacks map[string]*flyontime.Notification
@@ -51,6 +52,8 @@ func (s *Notifier) Commands() <-chan *flyontime.Command {
 
 		for msg := range rtm.IncomingEvents {
 			switch ev := msg.Data.(type) {
+			case *slack.ConnectedEvent:
+				s.selfID = ev.Info.User.ID
 			case *slack.MessageEvent:
 				s.handleMessageEvent(ev)
 			}
@@ -61,48 +64,98 @@ func (s *Notifier) Commands() <-chan *flyontime.Command {
 }
 
 func (s *Notifier) handleMessageEvent(m *slack.MessageEvent) {
-	if m.SubType == "message_replied" {
-		totalReplies := len(m.SubMessage.Replies)
-		lastReply := m.SubMessage.Replies[totalReplies-1]
-		reply, ok := s.messages[messageKey{lastReply.User, lastReply.Timestamp}]
-		if !ok {
-			return
-		}
-		atts := m.SubMessage.Attachments
-		if len(atts) > 0 && atts[0].CallbackID != "" {
-			s.handleReply(atts[0].CallbackID, reply.Text, m.SubMessage.ThreadTimestamp)
-			return
-		}
+	// There are three distinct type of messages that are handled:
+	// 1) IM (direct) messages
+	// 2) Replies to posted notifications
+	// 3) Mentions in the configured channel
+
+	if s.isIM(m.Channel) && m.User != "" && m.BotID == "" {
+		s.handleDirectMessage(m)
 		return
 	}
+	if m.Channel != s.ChannelID {
+		return
+	}
+	if m.SubType == "message_replied" {
+		s.handleReplyMessage(m)
+		return
+	}
+	if strings.HasPrefix(m.Msg.Text, fmt.Sprintf("<@%s> ", s.selfID)) {
+		s.handleMentionMessage(m)
+		return
+	}
+
 	// TODO(borshukov): We need some way to get rid of old messages.
 	s.messages[messageKey{m.User, m.Timestamp}] = m
 	return
 }
 
-func (s *Notifier) handleReply(callbackID, text, threadTimestamp string) {
+func (s *Notifier) handleDirectMessage(m *slack.MessageEvent) {
+	cmd, args := parseCommand(m.Msg.Text)
+	s.run(&flyontime.Command{Name: cmd, Args: args}, s.replyToIM(m.Channel))
+}
+
+func (s *Notifier) handleReplyMessage(m *slack.MessageEvent) {
+	totalReplies := len(m.SubMessage.Replies)
+	lastReply := m.SubMessage.Replies[totalReplies-1]
+	reply, ok := s.messages[messageKey{lastReply.User, lastReply.Timestamp}]
+	if !ok {
+		return
+	}
+	atts := m.SubMessage.Attachments
+	if len(atts) == 0 || atts[0].CallbackID == "" {
+		return
+	}
+	callbackID := atts[0].CallbackID
 	n, ok := s.callbacks[callbackID]
 	if !ok {
 		return
 	}
-	cmd, args := parseCommand(text)
+	cmd, args := parseCommand(reply.Text)
+	s.run(&flyontime.Command{Name: cmd, Args: args, Job: &n.Job}, s.replyToThread(m.SubMessage.ThreadTimestamp))
+}
 
+func (s *Notifier) handleMentionMessage(m *slack.MessageEvent) {
+	words := strings.Split(strings.TrimSpace(m.Msg.Text), " ")
+	if len(words) < 2 {
+		// TODO(borshukov): Log.
+		return
+	}
+
+	cmd, args := words[1], words[2:]
+	s.run(&flyontime.Command{Name: cmd, Args: args}, s.replyToThread(m.ThreadTimestamp))
+}
+
+func (s *Notifier) run(c *flyontime.Command, reply replyFunc) {
 	go func() {
 		responses := make(chan string)
+		c.Responses = responses
 		// Send the command.
-		s.commands <- &flyontime.Command{
-			Name:      cmd,
-			Args:      args,
-			Job:       &n.Job,
-			Responses: responses,
-		}
+		s.commands <- c
 		// And post each response as a message.
 		for r := range responses {
-			s.slack.PostMessage(s.ChannelID, r, slack.PostMessageParameters{
-				ThreadTimestamp: threadTimestamp,
-			})
+			reply(r)
 		}
 	}()
+}
+
+type replyFunc func(reply string)
+
+func (s *Notifier) replyToThread(ts string) replyFunc {
+	return func(reply string) {
+		s.slack.PostMessage(s.ChannelID, reply, slack.PostMessageParameters{
+			ThreadTimestamp: ts,
+			Markdown:        true,
+		})
+	}
+}
+
+func (s *Notifier) replyToIM(channelID string) replyFunc {
+	return func(reply string) {
+		s.slack.PostMessage(channelID, reply, slack.PostMessageParameters{
+			Markdown: true,
+		})
+	}
 }
 
 func (s *Notifier) Notify(ctx context.Context, n *flyontime.Notification) error {
@@ -128,6 +181,23 @@ func (s *Notifier) Notify(ctx context.Context, n *flyontime.Notification) error 
 	}
 	s.callbacks[callbackID] = n
 	return nil
+}
+
+func (s *Notifier) isIM(channelID string) bool {
+	logger := s.Logger.Session("is-im")
+	// TODO(borshukov): Cache the result of GetIMChannels.
+	ims, err := s.slack.GetIMChannels()
+	if err != nil {
+		logger.Error("get-im-channels.fail", err)
+		return false
+	}
+
+	for _, im := range ims {
+		if im.ID == channelID {
+			return true
+		}
+	}
+	return false
 }
 
 func (s *Notifier) updateBotUser() {
